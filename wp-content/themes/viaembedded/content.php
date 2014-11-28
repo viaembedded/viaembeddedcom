548312c26e4085b4693ba00d74e7055f605b9586
Gergely Imreh <imrehg@gmail.com>
2014-11-28 11:49:51 +0800

    update .gitignore and add _s base theme (unmodified)

---
+++ wp-content/themes/viaembedded/content.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @package VIA Embedded
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
+
+		<?php if ( 'post' == get_post_type() ) : ?>
+		<div class="entry-meta">
+			<?php viaembedded_posted_on(); ?>
+		</div><!-- .entry-meta -->
+		<?php endif; ?>
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<?php
+			/* translators: %s: Name of current post */
+			the_content( sprintf(
+				__( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'viaembedded' ),
+				the_title( '<span class="screen-reader-text">"', '"</span>', false )
+			) );
+		?>
+
+		<?php
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . __( 'Pages:', 'viaembedded' ),
+				'after'  => '</div>',
+			) );
+		?>
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+		<?php viaembedded_entry_footer(); ?>
+	</footer><!-- .entry-footer -->
+</article><!-- #post-## -->

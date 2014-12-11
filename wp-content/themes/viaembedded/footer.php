1ba7e1b66412d651811eec91ccc53a994e0e848f
Gergely Imreh <imrehg@gmail.com>
2014-12-11 15:50:17 +0800

    theme add 'full width' version of the design to the template
    This is porting some of the parts from "[Full Width _s][fwu]".
    
    [fwu]: http://sdavis2702.github.io/full-width_s/ "Full-width Underscores on Github"

--- wp-content/themes/viaembedded/footer.php
+++ wp-content/themes/viaembedded/footer.php
@@ -8,28 +8,33 @@
  */
 ?>
 
-	</div><!-- #content -->
+		</div><!-- #content -->
+	</div><!-- .main -->
+</div><!-- #content-row -->
 
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<div>
-				<strong>Americas</strong><br>
-				<a href="tel:+1-510-687-4688">+1-510-687-4688</a><br>
-				<a href="mailto:embedded@viatech.com">embedded@viatech.com</a>
-			</div>
-			<div>
-				<strong>Europe</strong><br>
-				<a href="tel:+49-228-688565-0">+49-228-688565-0</a><br>
-				<a href="mailto:embedded@viatech.eu">embedded@viatech.eu</a>
-			</div>
-			<div>
-				<strong>Rest of the World</strong><br>
-				<a href="tel:+886-2-2218-5452">+886-2-2218-5452</a><br>
-				<a href="mailto:embedded@via.com.tw">embedded@via.com.tw</a>
-			</div>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
+<div id="footer-row" class="full">
+	<div class="main">
+		<footer id="colophon" class="site-footer inner" role="contentinfo">
+			<div class="site-info">
+				<div>
+					<strong>Americas</strong><br>
+					<a href="tel:+1-510-687-4688">+1-510-687-4688</a><br>
+					<a href="mailto:embedded@viatech.com">embedded@viatech.com</a>
+				</div>
+				<div>
+					<strong>Europe</strong><br>
+					<a href="tel:+49-228-688565-0">+49-228-688565-0</a><br>
+					<a href="mailto:embedded@viatech.eu">embedded@viatech.eu</a>
+				</div>
+				<div>
+					<strong>Rest of the World</strong><br>
+					<a href="tel:+886-2-2218-5452">+886-2-2218-5452</a><br>
+					<a href="mailto:embedded@via.com.tw">embedded@via.com.tw</a>
+				</div>
+			</div><!-- .site-info -->
+		</footer><!-- #colophon -->
+	</div><!-- #main -->
+</div><!-- #footer-row -->
 
 <?php wp_footer(); ?>
 
--- wp-content/themes/viaembedded/header.php
+++ wp-content/themes/viaembedded/header.php
@@ -19,19 +19,25 @@
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="hfeed site">
-	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'viaembedded' ); ?></a>
+<div id="header-row" class="full">
+	<div class="main">
+		<?php do_action( 'before' ); ?>
+		<header id="masthead" class="site-header inner" role="banner">
+			<div class="site-branding">
+				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+			</div>
 
-	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
-		</div><!-- .site-branding -->
+			<nav id="site-navigation" class="main-navigation" role="navigation">
+				<h1 class="menu-toggle"><?php _e( 'Menu', 'fwu' ); ?></h1>
+				<div class="screen-reader-text skip-link"><a href="#content" title="<?php esc_attr_e( 'Skip to content', 'fwu' ); ?>"><?php _e( 'Skip to content', 'fwu' ); ?></a></div>
 
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="menu" aria-expanded="false"><?php _e( 'Primary Menu', 'viaembedded' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+				<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
+			</nav><!-- #site-navigation -->
+		</header><!-- #masthead -->
+	</div><!-- .main -->
+</div><!-- #header-row -->
 
-	<div id="content" class="site-content">
+<div id="content-row" class="full">
+	<div class="main">
+		<div id="content" class="site-content inner">

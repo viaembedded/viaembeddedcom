4a558b33193e1c20c80a8baf97863c8fe4233dd8
Gergely Imreh <imrehg@gmail.com>
2014-12-19 14:25:55 +0800

    theme: product summary sections alignment, closes #29
    Product thumb and highlights sections: better positioning,
    responsive behavior.

--- wp-content/themes/viaembedded/content-product.php
+++ wp-content/themes/viaembedded/content-product.php
@@ -7,56 +7,56 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	</header><!-- .entry-header -->
+  <header class="entry-header">
+    <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+  </header><!-- .entry-header -->
 
-	<div class="entry-content product-content">
-	  <!-- <div id="product-thumb"><?php the_post_thumbnail( $size, $attr ); ?></div> -->
-	  <div id="product-thumb">
-	  <div id="slides">
-	      <?php
-		 $thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'slideshow' );
-	         $thumb_url = $thumb[0];
-	      ?>
-	      <img src="<?php echo $thumb_url;?>">
+  <div class="entry-content product-content">
+    <div id="product-summary">
+      <div id="product-thumb">
+	<div id="slides">
+	  <?php
+	     $thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'slideshow' );
+	     $thumb_url = $thumb[0];
+	  ?>
+	  <img src="<?php echo $thumb_url;?>">
 	  <?php
 	     if (class_exists('MultiPostThumbnails')) {
-	        for($i=1; $i<5; $i++) {
-	 	   $thumb = MultiPostThumbnails::get_post_thumbnail_id('page', 'additional-image-'.$i, $post->ID);
-  	           $thumb_src = wp_get_attachment_image_src($thumb,'page-secondary-image-'.i.'-slideshow');
-	           $thumb_url = $thumb_src[0];
-	           if (strlen($thumb_url) > 0) {
-                     ?>
-	        <img src="<?php echo $thumb_url;?>">
-	             <?
-	           }
-		}
+	      for($i=1; $i<5; $i++) {
+		$thumb = MultiPostThumbnails::get_post_thumbnail_id('page', 'additional-image-'.$i, $post->ID);
+  	        $thumb_src = wp_get_attachment_image_src($thumb,'page-secondary-image-'.i.'-slideshow');
+	        $thumb_url = $thumb_src[0];
+	        if (strlen($thumb_url) > 0) {
+                ?>
+	           <img src="<?php echo $thumb_url;?>">
+	        <?
+	        }
+	      }
 	     }
 	  ?>
-	  </div>
-	  </div>
-		<div id="product-info">
-			<?php $shop_link = get_post_meta( get_the_ID(), 'shop-link', true );
-			if( ! empty( $shop_link ) ) {
-				echo "<div id=\"shop-link\">
+	</div><!-- #slides -->
+      </div><!-- #product-thumb -->
+      <div id="product-info">
+	<?php $shop_link = get_post_meta( get_the_ID(), 'shop-link', true );
+	      if( ! empty( $shop_link ) ) {
+	      echo "<div id=\"shop-link\">
 <a class=\"fasc-button fasc-size-medium fasc-type-flat fasc-rounded-medium fasc-ico-before dashicons-cart\" style=\"background-color: #007AC2; color: #ffffff;\" href=\"$shop_link\" data-fasc-style=\"background-color:#007AC2;color:#ffffff;\">Order Sample</a>
-</div>
-";
-			}
-			?>
-			<?php $highlights = get_post_meta( get_the_ID(), 'product-highlight', false );
-			if( ! empty( $highlights ) ) {
-			        echo "<div id=\"product-highlights-outer\">";
-				echo "<ul id=\"product-highlights\">";
-				foreach ($highlights as $line) {
-					echo "<li class=\"product-highlights-item\">$line</li>";
-				}
-				echo "</ul>";
-				echo "</div>";
-			}
-			?>
-		</div>
+</div>";
+	      }
+	?>
+	<?php $highlights = get_post_meta( get_the_ID(), 'product-highlight', false );
+	      if( ! empty( $highlights ) ) {
+	         echo "<div id=\"product-highlights-outer\">";
+		 echo "<ul id=\"product-highlights\">";
+		 foreach ($highlights as $line) {
+		   echo "<li class=\"product-highlights-item\">$line</li>";
+		 }
+		 echo "</ul>";
+		 echo "</div>";
+	      }
+	?>
+      </div><!-- #product-info -->
+    </div><!-- #product-summary -->
 		<?php the_content(); ?>
 		<?php
 			wp_link_pages( array(

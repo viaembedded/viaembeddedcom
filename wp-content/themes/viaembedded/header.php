a2822ed80e92d91855f0c6d07da5cc43e4e423c2
Gergely Imreh <imrehg@gmail.com>
2015-01-12 15:06:01 +0800

    theme: move breadcrumbs to the bottom of the page

--- wp-content/themes/viaembedded/header.php
+++ wp-content/themes/viaembedded/header.php
@@ -53,11 +53,6 @@
 	<div class="main">
 		<?php do_action( 'before' ); ?>
 		<header id="masthead" class="inner">
-                  <?php if ( function_exists('yoast_breadcrumb') ) {
-			echo "<div id=\"breadcrumbs-outer\">";
-			  yoast_breadcrumb('<p id="breadcrumbs">','</p>');
-			echo "</div>";
-			} ?>
 		       <div id="search-outer">
 			 <?php get_search_form(); ?>
 		       </div>

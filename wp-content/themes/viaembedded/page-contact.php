4fc4145722c8ccc3cf120edafafed55c45beb1b9
Gergely Imreh <imrehg@gmail.com>
2015-02-04 12:32:14 +0800

    theme: rewrite page-contact template header

--- wp-content/themes/viaembedded/page-contact.php
+++ wp-content/themes/viaembedded/page-contact.php
@@ -1,12 +1,7 @@
 <?php
 /*
  *
- * The template for displaying all product pages.
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site will use a
- * different template.
+ * The template for displaying the contact page.
  *
  * @package VIA Embedded
  */

6ccda3a881c508c957b20d477d7ddb1cf8510030
Gergely Imreh <imrehg@gmail.com>
2015-02-04 14:21:48 +0800

    theme: do not make slideshow out of a single featured image on the product pages

--- wp-content/themes/viaembedded/js/slider.js
+++ wp-content/themes/viaembedded/js/slider.js
@@ -1,23 +1,29 @@
 jQuery(document).ready(function ($) {
 
-    $("#slides").slidesjs({
-	width: 400,
-	height: 275,
-	navigation: {
-	    active: false,
-	},
-	pagination: {
-	    active: false
-	},
-	play: {
-	    active: false,
-            effect: "slide",
-	    interval: 5000,
-	    auto: true,
-	    swap: true,
-	    pauseOnHover: true,
-	    restartDelay: 2500
-	}
-    });
+    var slideDOM = "#slides";
 
+    if ( $(slideDOM + " img").length > 1 ) {
+
+        $("#slides").slidesjs({
+	    width: 400,
+	    height: 275,
+	    navigation: {
+	        active: false
+	    },
+	    pagination: {
+	        active: false
+	    },
+	    play: {
+	        active: false,
+                effect: "slide",
+	        interval: 5000,
+	        auto: true,
+	        swap: true,
+	        pauseOnHover: true,
+	        restartDelay: 2500
+	    }
+        });
+    } else {
+        $(slideDOM).show();
+    };
 });

14d5e32a80e6aa8d7787f546186d78a911f2ae45
Gergely Imreh <imrehg@gmail.com>
2015-02-04 10:27:33 +0800

    theme: combine divs in header that have similar function and belong together

--- wp-content/themes/viaembedded/header.php
+++ wp-content/themes/viaembedded/header.php
@@ -45,22 +45,17 @@ endif;?>
 	      <?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
 	    </nav><!-- #site-navigation -->
 	  </div><!-- #site-navigation-outer -->
-	</div><!-- .main -->
-</div><!-- #header-row -->
-
-<div id="header-row" class="full">
-	<div class="main">
-		<?php do_action( 'before' ); ?>
-		<header id="masthead" class="inner">
-                  <?php if ( function_exists('yoast_breadcrumb') ) {
+	  <?php do_action( 'before' ); ?>
+	  <header id="masthead" class="inner">
+            <?php if ( function_exists('yoast_breadcrumb') ) {
 			echo "<div id=\"breadcrumbs-outer\">";
 			  yoast_breadcrumb('<p id="breadcrumbs">','</p>');
 			echo "</div>";
-			} ?>
-		       <div id="search-outer">
-			 <?php get_search_form(); ?>
-		       </div>
-		</header><!-- #masthead -->
+		} ?>
+	    <div id="search-outer">
+	    <?php get_search_form(); ?>
+	    </div>
+	   </header><!-- #masthead -->
 	</div><!-- .main -->
 </div><!-- #header-row -->
 

c1234f53b91c42d718963b7c8644a3782c858c77
Gergely Imreh <imrehg@gmail.com>
2015-02-26 16:08:40 +0800

    theme: styling comments and pingbacks

--- wp-content/themes/viaembedded/comments.php
+++ wp-content/themes/viaembedded/comments.php
@@ -25,7 +25,7 @@ if ( post_password_required() ) {
 	<?php if ( have_comments() ) : ?>
 		<h2 class="comments-title">
 			<?php
-				printf( _nx( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'viaembedded' ),
+				printf( _nx( 'One mention on the Web for &ldquo;%2$s&rdquo;', '%1$s mentions on the Web for &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'viaembedded' ),
 					number_format_i18n( get_comments_number() ), '<span>' . get_the_title() . '</span>' );
 			?>
 		</h2>
@@ -61,7 +61,7 @@ if ( post_password_required() ) {
 		// If comments are closed and there are comments, let's leave a little note, shall we?
 		if ( ! comments_open() && '0' != get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) :
 	?>
-		<p class="no-comments"><?php _e( 'Comments are closed.', 'viaembedded' ); ?></p>
+		<p class="no-comments"><?php // _e( 'Comments are closed.', 'viaembedded' ); ?></p>
 	<?php endif; ?>
 
 	<?php comment_form(); ?>

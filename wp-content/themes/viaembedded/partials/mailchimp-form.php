98a0dd20f2f8e52487a9457fc8bd5349f78b7ea6
Gergely Imreh <imrehg@gmail.com>
2015-09-30 15:55:19 +0800

    BSP signup: fix typo for ARTiGO A600

--- wp-content/themes/viaembedded/partials/mailchimp-form.php
+++ wp-content/themes/viaembedded/partials/mailchimp-form.php
@@ -51,7 +51,7 @@
 				    <label for="mce-group[16297]-16297-9">ARTiGO A900</label>
 			    </li>
 			    <li>
-				    <input type="checkbox" value="512" name="group[16297][1024]" id="mce-group[16297]-16297-10">
+				    <input type="checkbox" value="1024" name="group[16297][1024]" id="mce-group[16297]-16297-10">
 				    <label for="mce-group[16297]-16297-10">ARTiGO A600</label>
 			    </li>
 			    <li>
